be5d99756d774644b09d91499a8807e16f2c2eef
fabrisal7 <fabrisal@dei.unipd.it>
2021-04-19 12:06:22 +0100

    minor

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from os.path import join
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
-from data import adultcsv_loader
+from data import adultcsv_loader, compascsv_loader, ccdefaultcsv_loader
 from common import *
 from quapy.method.aggregative import CC, PCC, ACC, PACC, EMQ, HDy
 from common import Protocols
@@ -19,9 +19,6 @@ qp.environ['SAMPLE_SIZE'] = 100
 
 model_selection = False
 datasplit_repetitions = 5
-data_path = "datasets/adult.csv"
-data_loader = adultcsv_loader
-protected_attr = "gender"
 options = {
     'nprevs': 11,
     'nreps': 10,
@@ -55,6 +52,14 @@ def quantifiers():
     yield 'HDy', HDy
 
 
+# Define the quantifiers we would like to test
+# --------------------------------------------
+def datasets():
+    yield 'adult', "datasets/adult.csv", adultcsv_loader, "gender"
+    yield 'compas', "datasets/compas-scores-two-years.csv", compascsv_loader, "race"
+    # yield 'cc_default', "datasets/default of credit card clients.csv", ccdefaultcsv_loader, "SEX"
+
+
 # instantiate all quantifiers x classifiers (wrapped also within model selection if requested)
 def iter_quantifiers(model_selection=True):
     for (c_name, c, hyper), (q_name, q) in itertools.product(classifiers(), quantifiers()):
@@ -74,58 +79,59 @@ def iter_quantifiers(model_selection=True):
         yield name, q
 
 
-def dataset_name():
-    return pathlib.Path(data_path).name.replace('.csv', '')+'_'+protected_attr
-
 
 def run_name():
     options_par = '_'.join(f'{key}{options[key]}' for key in sorted(options.keys()))
-    return f'{dataset_name()}_{Q_name}_f{fname}_Run{run}_{protocol}_{options_par}_modsel{model_selection}.pkl'
+    return f'{dataset_name}_{Q_name}_f{fname}_Run{run}_{protocol}_{options_par}_modsel{model_selection}.pkl'
 
 
 # --------------------------------------------
 # Compute the experiments
 # --------------------------------------------
 
-X, y, A = data_loader(data_path, protected_attr=protected_attr)
 
 os.makedirs(result_dir, exist_ok=True)
 os.makedirs(table_dir, exist_ok=True)
 os.makedirs(plot_dir, exist_ok=True)
 
-results = []
-for run, (D1, D2, D3, AD1) in enumerate(gen_split_data(X, y, A, repetitions=datasplit_repetitions)):
-    pbar = tqdm(itertools.product(iter_quantifiers(model_selection), Protocols))
-    for (Q_name, Q), protocol in pbar:
-        pbar.set_description(f'{protocol}: {Q_name}')
 
-        result_path = os.path.join(result_dir, run_name())
-        if skip_already_computed and os.path.exists(result_path):
-            print(f'skipping {result_path}; already computed!')
-            results.append(Result.load(result_path))
-            continue
+for dataset_name, data_path, loader, protected in datasets():
+    dataset_name = f'{dataset_name}_{protected}'
+    X, y, A = loader(data_path, protected_attr=protected)
 
-        if protocol == Protocols.VAR_D1_PREV:
-            outs = eval_prevalence_variations_D1(D1, D2, D3, AD1, f, Q, **options)
-        elif protocol == Protocols.VAR_D1_PREVFLIP:
-            outs = eval_prevalence_variations_D1_flip(D1, D2, D3, AD1, f, Q, **options)
-        elif protocol == Protocols.VAR_D2_SIZE:
-            outs = eval_size_variations_D2(D1, D2, D3, f, Q, sample_sizes=None, nreps=options['nreps'])
-        elif protocol == Protocols.VAR_D2_PREV:
-            outs = eval_prevalence_variations_D2(D1, D2, D3, f, Q, **options)
-        elif protocol == Protocols.VAR_D3_PREV:
-            outs = eval_prevalence_variations_D3(D1, D2, D3, f, Q, **options)
+    results = []
+    for run, (D1, D2, D3, AD1) in enumerate(gen_split_data(X, y, A, repetitions=datasplit_repetitions)):
+        pbar = tqdm(itertools.product(iter_quantifiers(model_selection), Protocols))
+        for (Q_name, Q), protocol in pbar:
+            pbar.set_description(f'{dataset_name} - {protocol}: {Q_name}')
 
-        outs.set('Q_name', Q_name)
-        outs.set('run', run)
-        outs.save(result_path)
-        results.append(outs)
+            result_path = os.path.join(result_dir, run_name())
+            if skip_already_computed and os.path.exists(result_path):
+                print(f'skipping {result_path}; already computed!')
+                results.append(Result.load(result_path))
+                continue
+
+            if protocol == Protocols.VAR_D1_PREV:
+                outs = eval_prevalence_variations_D1(D1, D2, D3, AD1, f, Q, **options)
+            elif protocol == Protocols.VAR_D1_PREVFLIP:
+                outs = eval_prevalence_variations_D1_flip(D1, D2, D3, AD1, f, Q, **options)
+            elif protocol == Protocols.VAR_D2_SIZE:
+                outs = eval_size_variations_D2(D1, D2, D3, f, Q, sample_sizes=None, nreps=options['nreps'])
+            elif protocol == Protocols.VAR_D2_PREV:
+                outs = eval_prevalence_variations_D2(D1, D2, D3, f, Q, **options)
+            elif protocol == Protocols.VAR_D3_PREV:
+                outs = eval_prevalence_variations_D3(D1, D2, D3, f, Q, **options)
+
+            outs.set('Q_name', Q_name)
+            outs.set('run', run)
+            outs.save(result_path)
+            results.append(outs)
 
 
 # -------------------------
 # Generate tables and plots
 # -------------------------
 for protocol in Protocols:
-    generate_tables(protocol, results, table_path=join(table_dir, f'tab_{protocol}_{dataset_name()}.tex'))
-    generate_plots(protocol, results, plotdir=join(plot_dir, dataset_name()))
+    generate_tables(protocol, results, table_path=join(table_dir, f'tab_{protocol}_{dataset_name}.tex'))
+    generate_plots(protocol, results, plotdir=join(plot_dir, dataset_name))
 
